20093f15682a65d60defb8fead541391736879fd
winnerthetechgenie <onubawinner042@gmail.com>
2024-03-18 01:19:19 +0100

    major update

--- main_copy.py
+++ main_copy.py
@@ -4,54 +4,58 @@ import matplotlib.pyplot as plt
 
 def main():
     
-    # Creating a list of reviews (serves as the database in this use case)
-    list_of_reviews = []
+    def remark(com):
+        if com > 0.25:
+            return 'positive'
+        if com < -0.25:
+            return 'negative'
+        else:
+            return 'neutral'
     
+    if 'list_of_reviews' not in st.session_state:
+        st.session_state.list_of_reviews={}
     # Disply structure
     st.title('TEXT SENTIMENT ANALYZER')
     col = st.columns(2)
 
     # input section
     with col[0].form(key='input form'):
-        user_input = st.text_input('Review product')
+        user_input = st.text_input('Please leave a review')
         
         button = st.form_submit_button(label='Submit')
         
         if button:
-            list_of_reviews.append(user_input)
+            
+                # sentiment analyzer
+            sia = SentimentIntensityAnalyzer()
+            intensity = sia.polarity_scores(user_input)  
+            
+            keys = list(intensity.keys())
+            values = list(intensity.values()) 
+            
+            new_sentiment = remark(intensity['compound'])
+            st.session_state.list_of_reviews[user_input] = new_sentiment
 
-    st.write(f'Review Count: {len(list_of_reviews)}')
+    st.write(f'Review Count: {len(st.session_state.list_of_reviews)}')
+    
     
     #reviews section
     st.subheader('REVIEWS')   
-    for i in list_of_reviews:
-        st.write('*',i)   
+    for i,j in st.session_state.list_of_reviews.items():
+        st.write('*',i,'[',j,']')   
          
-    # sentiment analyzer
-    sia = SentimentIntensityAnalyzer()
-    output = sia.polarity_scores(user_input)
-    st.write(output)
+
 
     #if col[0].button('Submit'):
     #    list_of_reviews.append(input)
     #    st.write(f'Review Count: {len(list_of_reviews)}')
          
 
-    
-    
-
-    keys = list(output.keys())
-    values = list(output.values())
 
 
     # Plotting the bar chart
 
-    colors = ['red' if value <0 else 'blue' for value in values]
-    fig, ax = plt.subplots(figsize=(5,4))
-    ax.bar(keys, values, color=colors)
-    ax.axhline(0, color='gray',linestyle='--', linewidth=0.5)
-    ax.set_ylim(-1,1)
-    col[1].pyplot(fig)
+
 
     # COMMENT
     st.text('Note: The compound value shows the sentiment of the comment on a scale of -1 and 1.')
